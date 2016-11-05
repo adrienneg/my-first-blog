0c34a08b3fc101bd226f2e23b120660019098f2f
Adrienne <adrienne.gifford@gmail.com>
2016-11-05 14:38:33 -0700

    fixing disallowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k$mio=ayfi)t9txj!z3@sltmn)#r-8fv$xbkk0z6@4y7*^$53@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['adriennegifford.pythonanywhere.com']
 
 
 # Application definition
